992a70ffc038dab60d738078c680ce5f01b58a67
Maksim <the.butterfly.incorporated@gmail.com>
2020-03-12 19:52:45 +0300

    First commit 4

--- index.js
+++ index.js
@@ -52,4 +52,4 @@ app.post('/upload', uploadMemory.array('file'), (req, res, next) => {
 	res.json({ succeed: true });
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'));
